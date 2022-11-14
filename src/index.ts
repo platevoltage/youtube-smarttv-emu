a15be7e64816a14bef61ebea4d7da3c6841bb4be
antieatingactivist <antieatingactivist@gmail.com>
2022-11-14 11:42:00 -0800

    youtube loads in window

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
-import { app } from 'electron';
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
+const createMainPopup = () => {
+  const win = new BrowserWindow({
+    width: 550,
+    height: 500,
+    visualEffectState: "active",
+    vibrancy: 'sidebar',
+    resizable: false,
+    maximizable: false,
+    movable: false,
+    // titleBarStyle: "hidden",
+    useContentSize: true,
+    frame: false,
+    // show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      // preload: path.join(__dirname, 'preload.js')
+    }
+  });
 
+  win.loadURL('https://youtube.com');
+  return win
+      
+};
 
-let data = {};
 
 
 app.whenReady().then(async () => {
 
-  
+  createMainPopup();
   // note: your contextMenu, Tooltip and Title code will go here!
 })
 
